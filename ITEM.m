6e0aedd4306e7dd670612de6fdd5e532d2e1e8c1
Oleh Melnyk <oleh.l.melnyk@gmail.com>
2021-02-11 16:19:51 +0100

    ITEM and test script

---
+++ ITEM.m
@@ -0,0 +1,40 @@
+function z = ITEM(gradf,mu,L,x0,N)
+
+if (mu ~= 0)
+    q = mu/L;
+
+    Ak = 0;
+    zk = x0;
+    xk = x0;
+
+    for i = 1:N
+        Ak1 = ( (1+q)*Ak+2*(1+sqrt((1+Ak)*(1+q*Ak)) ))/(1-q)^2; 
+        betak  	= Ak/(1-q)/Ak1;
+        deltak 	= 0.5 * ( (1-q)^2*Ak1 -(1+q)*Ak)/(1+q+q*Ak);
+        yk  = betak * xk + (1-betak) * zk;
+        grk = gradf(yk);
+        xk = yk - grk/L;
+        zk = (1 - q*deltak)*zk + q* deltak*yk - deltak*grk/L;
+    end
+    z = zk;
+else
+    thetak = 1;
+
+    yk = x0;
+    xk = x0;
+
+    for i = 1:N
+        grk = gradf(xk);
+        yk1  =  xk - grk/L;
+        if (i == N)
+            thetak1 = 0.5*(1+sqrt(1 + 8*thetak^2));
+        else
+            thetak1 = 0.5*(1+sqrt(1 + 4*thetak^2));
+        end
+        xk = yk1 + (thetak - 1)*(yk1 - yk)/thetak1 - thetak * grk /thetak1 / L;
+        yk = yk1;
+        thetak = thetak1;
+    end
+    z = xk;
+end
+end
---
+++ test.m
@@ -0,0 +1,31 @@
+% x0 = [0;0;1;0];
+% f = @(x) 0.5*norm(x - x0)^2;
+% gradf = @(x) x - x0;
+% x = ITEM(gradf,0,1,[1;0;0;0],1000)
+
+
+m = 30;
+n = 100;
+A = randn(m,n);
+x = [ones(5,1) ; zeros(95,1)];
+b = A * x;
+
+lambda = 10^0;
+eps1 = 10^-5;
+eps2 = 10^-5;
+x0 = randn(n,1);
+mu = 0;
+L = lambda/ sqrt(eps2) + 1.0/sqrt(eps1);
+
+gradf= @(x) A' * (A*x-b)/ sqrt( norm(A*x-b)^2 + eps1) + lambda * x ./ sqrt( abs(x).^2 + eps2);
+
+xr = ITEM(gradf,mu,L,x0,10000)
+
+bn = b; % + sigma*normrnd(0,1,m,1);
+lambda = 10^1;
+OPTIONS = set_parameters;
+OPTIONS.tol_I = 1.0e-6;
+[xr2,beta,runhist_I,runhist_II]= square_root_PMM(A,bn,m,n,lambda,OPTIONS,'l1');
+
+norm(xr - x)/norm(x)
+norm(xr2 - x)/norm(x)
